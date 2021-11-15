a103b89c79af78f4bb93aa1ec5d0b26079b65978
fab1214 <88295561+fab1214@users.noreply.github.com>
2021-11-15 16:18:47 -0500

    add handlebar files and build page template

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,11 @@ const router = require('express').Router();
 //import js files from api folder
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes');
+
 //prefixes routes to be /api/users in URL
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
