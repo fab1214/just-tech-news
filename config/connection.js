87eeb53b17e5d7ec882676d30da138b0650af88c
fab1214 <88295561+fab1214@users.noreply.github.com>
2021-11-15 14:35:08 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.jawsdb-silhouetted-87412) {
-  sequelize = new Sequelize(process.env.jawsdb-silhouetted-87412);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
